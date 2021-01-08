ceaedc7631d980eb7d564f62e6cffa33ba02dd9e
Zhue Xia <dhenys.1002@gmail.com>
2021-01-08 20:07:13 +0700

    Create file first
    New Project

---
+++ BAB02/programpertamac.c
@@ -0,0 +1,18 @@
+/*
+Program 2.1
+Nama File : Lat-21.c
+Programmer : Dhn-nys
+*/
+
+//program Pertama
+#include <stdio.h>
+#include "conio.h"
+
+int main (int argc, char *argv[])
+{
+printf("Halo, ini program pertama\n");
+printf("\nSaya belajar bahasa C\nDi\"UNIKOM\"\nBandung");
+printf("\n\n");
+system("PAUSE");
+return 0;
+}
