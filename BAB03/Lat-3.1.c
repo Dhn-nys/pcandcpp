9dbc202b0fbfd09095e214ac7e4e8077676dc1b6
Dhn-nys <dhenys.1002@gmail.com>
2021-01-19 02:15:06 +0700

    Create Lat-3.1.c
    Type data C

---
+++ BAB03/Lat-3.1.c
@@ -0,0 +1,17 @@
+/*
+ Program 3.1
+ Nama File : Lat-3.1.c
+ Programmer : Adam Mukharil Bachtiar, S.Kom.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	printf("Memorial dari tipe data char : %i \n", sizeof(char));
+	printf("Memorial dari tipe data unsigned char : %i \n", sizeof(unsigned char));
+	printf("Memorial dari tipe data signed char : %i \n", sizeof(signed char));
+	system("PAUSE");
+	return 0;
+}
