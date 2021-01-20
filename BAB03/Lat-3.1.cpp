cfb85e6863bd326c4239ab0e8afae43a053943b4
Dhn-nys <dhenys.1002@gmail.com>
2021-01-20 16:16:44 +0700

    Create Lat-3.1.cpp
    Type data C++

---
+++ BAB03/Lat-3.1.cpp
@@ -0,0 +1,21 @@
+/*
+ Program 3.1
+ Nama File : Lat-3.1.cpp
+ Programmer : Adam Mukharil Bachtiar, S.Kom.
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+int main (int argc, char *argv[])
+{
+	cout<<"Memorial dari tipe data char : "<<sizeof(char);
+    cout<<"\n\n";
+	cout<<"Memorial dari tipe data unsigned char : "<<sizeof(unsigned char);
+    cout<<"\n\n";
+	cout<<"Memorial dari tipe data signed char : "<<sizeof(signed char);
+    cout<<"\n\n";
+	system("PAUSE");
+	return 0;
+}
